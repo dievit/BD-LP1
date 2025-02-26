d0f87612e315797e96103a9da88901404eb8b649
Diego Vitvicki <drmv09@gmail.com>
2025-02-25 21:57:07 -0300

    fix: edit exercise 2

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -4,6 +4,9 @@ public class Exercise2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o total de eleitores: ");
+        int eleitores = scanner.nextInt();
+
         System.out.println("Digite o total de votos em branco: ");
         int brancos = scanner.nextInt();
 
@@ -15,12 +18,13 @@ public class Exercise2 {
 
         System.out.println();
 
-        int totalEleitores = brancos + nulos + validos;
-        double percentualBrancos = (brancos * 100.0) / totalEleitores;
-        double percentualNulos = (nulos * 100.0) / totalEleitores;
-        double percentualValidos = (validos * 100.0) / totalEleitores;
+        int totalEleitoresValidos = brancos + nulos + validos;
+        double percentualBrancos = (brancos * 100.0) / eleitores;
+        double percentualNulos = (nulos * 100.0) / eleitores;
+        double percentualValidos = (validos * 100.0) / eleitores;
 
-        System.out.println("Total de eleitores: " + String.format("%.2f", totalEleitores));
+        System.out.println("Total de eleitores: " + String.format("%.2f", eleitores));
+        System.out.println("Total de Eleitores Válidos" + String.format("%.2f", totalEleitoresValidos));
         System.out.println("Percentual de votos em branco: " + String.format("%.2f", percentualBrancos + "%"));
         System.out.println("Percentual de votos nulos: " + String.format("%.2f", percentualNulos + "%"));
         System.out.println("Percentual de votos válidos: " + String.format("%.2f", percentualValidos + "%"));
